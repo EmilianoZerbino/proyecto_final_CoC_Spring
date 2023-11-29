1bb115cab49445e79ca7575e49885ad2a0562290
Emiliano Zerbino <emilianomzerbino@gmail.com>
2023-11-28 22:15:37 -0600

    Carga inicial del Proyecto

---
+++ src/main/java/com/FlySky/FlySkyApplication.java
@@ -0,0 +1,13 @@
+package com.FlySky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlySkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlySkyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FlySky/FlySkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlySky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlySkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
